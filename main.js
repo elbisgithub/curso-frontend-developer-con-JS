d5f6fa71c73d8993a81488ad01f36e3d5fdebed6
elbisgithub <aluprof.elbismarmol@gmail.com>
2022-08-08 09:30:12 -0500

    Se fusiona este menu mobile con el elemento del ucono hamburguesa,         para que aparezca cuando se vea en un dispositivo móvil

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+
+const menuIconBurguer=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+
+//*Muestra y oculta el menú escritorio en el modo desktop* //
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+//*Muestra y oculta el menú hamburguesa en el modo móvil* //
+menuIconBurguer.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
