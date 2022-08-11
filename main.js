3834c4dfa1c6da4f69908d861b58d276645fc909
elbisgithub <aluprof.elbismarmol@gmail.com>
2022-08-11 00:21:17 -0500

    Se agrega la etiqueta semántica Aside, que desplega los productos del carrito de compras, así como, las funciones y condicionales para que se abran y cierren los menues de la tienda online

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ const desktopMenu=document.querySelector('.desktop-menu');
 const menuIconBurguer=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
 
+//const asideProducts=document.querySelector('.navbar-shopping-cart');
+//const cartIcon=document.querySelector('.product-detail');
+
+const asideProducts=document.querySelector('.product-detail');
+const cartIcon=document.querySelector('.navbar-shopping-cart');
+
 //*Muestra y oculta el menú escritorio en el modo desktop* //
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    const isAsideClosed=asideProducts.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        asideProducts.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -15,5 +26,22 @@ function toggleDesktopMenu(){
 menuIconBurguer.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
+    const isAsideClosed=asideProducts.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        asideProducts.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+//*Muestra y oculta el Aside sobre el pedido hecho en el carrito de compras* //
+cartIcon.addEventListener('click', toggleCartAside);
+
+function toggleCartAside(){
+    const isMobileMenuClosed=mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    asideProducts.classList.toggle('inactive');
 }
